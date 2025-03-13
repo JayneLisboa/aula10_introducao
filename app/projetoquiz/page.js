24f6f6c862d19bc7474a37010e7cf8929e7fa52c
JayneLisboa <jaynelins15@gmail.com>
2025-03-13 16:13:17 -0300

    quiz bandeira v1

--- app/projetoquiz/page.js
+++ app/projetoquiz/page.js
@@ -1,5 +1,6 @@
 'use client'
 import { useState } from "react";
+import "./projeto.css"
 
 function quizbandeiras(){
     
@@ -7,7 +8,6 @@ function quizbandeiras(){
     const [b2, alterab2] = useState(0)
     const [b3, alterab3] = useState(0)
     const [b4, alterab4] = useState(0)
-    const [b5, alterab5] = useState(0)
 
     const [mostrab1, alteraMostrab1] = useState(true)
     const [mostrab2, alteraMostrab2] = useState(false)
@@ -15,13 +15,15 @@ function quizbandeiras(){
     const [mostrab4, alteraMostrab4] = useState(false)
     const [mostrab5, alteraMostrab5] = useState(false)
 
+    const [mostraResposta, alteraMostraResposta] = useState(false)
+
 
     const [TotalPontos, alteraTotalPontos] = useState(0)
     
-    function somaPontos(){
+    function somaPontos(valor){
 
         let totalAtualizado = 0
-        totalAtualizado = b1+b2+b3+b4+b5
+        totalAtualizado = b1+b2+b3+b4+valor
         
         alteraTotalPontos(totalAtualizado)
 
@@ -33,17 +35,24 @@ function quizbandeiras(){
     
     return ( 
         <div>
-            <h1>Quiz Bandeiras de cada país</h1>
+            <h1>QUIZ BANDEIRAS</h1>
 
             {
                 mostrab1 == true &&
                <div>
-                    <h2> Descubra a bandeira</h2>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/275px-Flag_of_Brazil.svg.png"></img>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab1(0); alteraMostrab1(false); alteraMostrab2(true)}}/> Brassil </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab1(0); alteraMostrab1(false); alteraMostrab2(true)}}/> Basil</p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab1(0); alteraMostrab1(false); alteraMostrab2(true)}}/> Drasil</p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab1(1); alteraMostrab1(false); alteraMostrab2(true)}}/> Brasil</p>
+                    <h2 className="titulo"> Descubra a bandeira</h2>
+                    <img className="img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/125px-Flag_of_Brazil.svg.png"></img>
+                  <div className="divisao">
+
+                    <p onClick={ ()=> {alterab1(0); alteraMostrab1(false); alteraMostrab2(true)}}> <strong>Brassil</strong> </p>
+                    <p onClick={ ()=> {alterab1(0); alteraMostrab1(false); alteraMostrab2(true)}}> <strong>Basil</strong></p>
+                  </div>
+                  <div className="divisao">
+
+                    <p onClick={ ()=> {alterab1(0); alteraMostrab1(false); alteraMostrab2(true)}}> <strong>Drasil</strong></p>
+                    <p onClick={ ()=> {alterab1(1); alteraMostrab1(false); alteraMostrab2(true)}}> <strong>Brasil</strong></p>
+
+                  </div>
                 </div>
             }
 
@@ -51,12 +60,17 @@ function quizbandeiras(){
             {
                 mostrab2 == true &&
                 <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Flag_of_Burkina_Faso.svg/125px-Flag_of_Burkina_Faso.svg.png"></img>
-                    <h2> Descubra a bandeira</h2>
-                    <p> <input name=" b2" type="radio" onChange={ ()=> {alterab2(0); alteraMostrab2(false); alteraMostrab3(true)}}/>Gana</p>
-                    <p> <input name=" b2" type="radio" onChange={ ()=> {alterab2(0); alteraMostrab2(false); alteraMostrab3(true)}}/>Camarões</p>
-                    <p> <input name=" b2" type="radio" onChange={ ()=> {alterab2(1); alteraMostrab2(false); alteraMostrab3(true)}}/>Burkina Fasso</p>
-                    <p> <input name=" b2" type="radio" onChange={ ()=> {alterab2(0); alteraMostrab2(false); alteraMostrab3(true)}}/>Moçambique</p>
+                    <h2 className="titulo"> Descubra a bandeira</h2>
+                    <img className="img"src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Flag_of_Burkina_Faso.svg/125px-Flag_of_Burkina_Faso.svg.png"></img>
+                    <div className="divisao">
+                        <p onClick={ ()=> {alterab2(0); alteraMostrab2(false); alteraMostrab3(true)}}><strong>Gana</strong></p>
+                        <p onClick={ ()=> {alterab2(0); alteraMostrab2(false); alteraMostrab3(true)}}><strong>Camarões</strong></p>
+                   </div>
+
+                    <div className="divisao">
+                        <p onClick={ ()=> {alterab2(1); alteraMostrab2(false); alteraMostrab3(true)}}><strong>Burkina Fasso</strong></p>
+                        <p onClick={ ()=> {alterab2(0); alteraMostrab2(false); alteraMostrab3(true)}}><strong>Moçambique</strong></p>
+                    </div>
                 </div>
 
             }
@@ -65,12 +79,21 @@ function quizbandeiras(){
             {
                 mostrab3 == true &&
                 <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Flag_of_Serbia.svg/125px-Flag_of_Serbia.svg.png"></img>
-                    <h2> Descubra a bandeira </h2>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab3(0); alteraMostrab3(false); alteraMostrab4(true)}}/>Croácia</p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab3(1); alteraMostrab3(false); alteraMostrab4(true)}}/>Sérvia</p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab3(0); alteraMostrab3(false); alteraMostrab4(true)}}/>Russia</p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab3(0); alteraMostrab3(false); alteraMostrab4(true)}}/>Slováquia</p>
+                    <h2 className="titulo"> Descubra a bandeira </h2>
+                    <img className="img"src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Flag_of_Serbia.svg/125px-Flag_of_Serbia.svg.png"></img>
+                    <div className="divisao">
+
+                        <p onClick={ ()=> {alterab3(0); alteraMostrab3(false); alteraMostrab4(true)}}><strong>Croácia</strong></p>
+                        <p onClick={ ()=> {alterab3(1); alteraMostrab3(false); alteraMostrab4(true)}}><strong>Sérvia</strong></p>
+
+                    </div>
+
+                    <div className="divisao">
+
+                        <p onClick={ ()=> {alterab3(0); alteraMostrab3(false); alteraMostrab4(true)}}><strong>Russia</strong></p>
+                        <p onClick={ ()=> {alterab3(0); alteraMostrab3(false); alteraMostrab4(true)}}><strong>Slováquia</strong></p>
+
+                    </div>
 
                 </div>
             }
@@ -78,12 +101,23 @@ function quizbandeiras(){
             {
                 mostrab4 == true &&
                 <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/125px-Flag_of_Italy.svg.png"></img>
-                    <h2> Descubra a bandeira</h2>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab4(0); alteraMostrab4(false); alteraMostrab5(true)}}/>França </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab4(1); alteraMostrab4(false); alteraMostrab5(true)}}/>Itália </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab4(0); alteraMostrab4(false); alteraMostrab5(true)}}/>México </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab4(0); alteraMostrab4(false); alteraMostrab5(true)}}/>Austria</p>
+                    
+                    <h2 className="titulo"> Descubra a bandeira</h2>
+                    <img className="img"src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/125px-Flag_of_Italy.svg.png"></img>
+
+                    <div className="divisao">
+
+                    <p onClick={ ()=> {alterab4(0); alteraMostrab4(false); alteraMostrab5(true)}}><strong>França</strong> </p>
+                    <p onClick={ ()=> {alterab4(1); alteraMostrab4(false); alteraMostrab5(true)}}><strong>Itália </strong></p>
+
+                    </div>
+
+                    <div className="divisao">
+
+                    <p onClick={ ()=> {alterab4(0); alteraMostrab4(false); alteraMostrab5(true)}}><strong>México </strong></p>
+                    <p onClick={ ()=> {alterab4(0); alteraMostrab4(false); alteraMostrab5(true)}}><strong>Austria</strong><strong></strong></p>
+
+                    </div>
 
                 </div>
             }
@@ -91,17 +125,34 @@ function quizbandeiras(){
             {
                 mostrab5 == true &&
                 <div>
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Flag_of_Vatican_City_%282023%E2%80%93present%29.svg/125px-Flag_of_Vatican_City_%282023%E2%80%93present%29.svg.png"></img>
-                    <h2> Descubra a bandeira</h2>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab5(0); alteraMostrab5(false)}}/>Chile </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab5(0); alteraMostrab5(false)}}/>irã </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab5(1); alteraMostrab5(false)}}/>Vaticano </p>
-                    <p> <input name=" b1" type="radio" onChange={ ()=> {alterab5(0); alteraMostrab5(false)}}/>Chipre</p>
+                    <h2 className="titulo"> Descubra a bandeira</h2>
+                    <img className="img"src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Flag_of_Vatican_City_%282023%E2%80%93present%29.svg/125px-Flag_of_Vatican_City_%282023%E2%80%93present%29.svg.png"></img>
+                    <div className="divisao">
+
+                        <p onClick={ ()=> {alteraMostrab5(false); alteraMostraResposta(true); somaPontos(0)}}><strong>Chile</strong> </p>
+                        <p onClick={ ()=> {alteraMostrab5(false); alteraMostraResposta(true); somaPontos(0)}}><strong>irã </strong></p>
+
+                    </div>
+
+                    <div className="divisao">
+
+                        <p onClick={ ()=> {alteraMostrab5(false); alteraMostraResposta(true); somaPontos(1)}}><strong>Vaticano</strong> </p>
+                        <p onClick={ ()=> {alteraMostrab5(false); alteraMostraResposta(true); somaPontos(0)}}><strong>Chipre</strong></p>
+
+                    </div>
+
+                </div>
+            }   
+
+            {
+                mostraResposta == true && 
+                <div>
+
+                    <p className="resposta"> Você acertou {TotalPontos} de 5!</p>
 
                 </div>
             }
 
-                <button onClick={()=> somaPontos()}>Enviar</button>
                 
                 
             
